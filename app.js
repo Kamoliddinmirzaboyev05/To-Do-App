38be68799839e13fa68841100abf4b2cd8c0efe9
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-01-09 22:59:46 +0500

    Update

--- app.js
+++ app.js
@@ -5,26 +5,87 @@ const tasks = document.querySelector(".tasks");
 const complatedMyDay = document.querySelector(".complatedMyDay");
 const mayDayRight = document.querySelector("#mayDayRight");
 const complatedTasks = document.querySelector(".complated-tasks");
+const searchInput = document.querySelector(".searchInput");
 const checkedTask = document.querySelectorAll(".checkedTask");
+
 var myDayDB = JSON.parse(localStorage.getItem("myDayDB"))
   ? JSON.parse(localStorage.getItem("myDayDB"))
   : [];
 
-//   Taskni HTML ga yozib beradigan funksiya
-
+var searchTaskDB = JSON.parse(localStorage.getItem("searchTaskDB"))
+  ? JSON.parse(localStorage.getItem("searchTaskDB"))
+  : [];
+searchTaskDB = [...myDayDB];
 const writeTask = () => {
   tasks.innerHTML = "";
-  myDayDB.forEach((newtask) => {
+  searchTaskDB.forEach((newtask) => {
     tasks.innerHTML += `
-            <div class="task ${newtask.checkedClass}">
-              <div class="task-text">
-                <div onclick="checked(${newtask.id})" class="check"></div>
-                <p>${newtask.task}</p>
-              </div>
-              <i class="fa-regular fa-star"></i>
-            </div>`;
+              <div class="task ${newtask.checkedClass}">
+                <div class="task-text">
+                  <div onclick="checked(${newtask.id})" class="check"><i class="fa-regular fa-circle ${newtask.notCheck}"></i>
+                  <i class="fa-solid fa-circle-check ${newtask.checkTask}"></i>
+                  </div>
+                  <p>${newtask.task}</p>
+                </div>
+                <div class = "task-btns">
+                <i onclick="deleteTask(${newtask.id})" class="fa-solid fa-trash trash"></i>
+                <i class="fa-regular fa-star"></i>
+                </div>
+              </div>`;
   });
 };
+writeTask();
+// Sanani ko'rsatib turuvchi funksiya
+var date = new Date();
+
+const weekDay = document.querySelector(".weekDay");
+const month = document.querySelector(".month");
+const monthDay = document.querySelector(".monthDay");
+const hour = document.querySelector(".hour");
+const minut = document.querySelector(".minut");
+const second = document.querySelector(".second");
+var week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+var digitalClock = () => {
+  hour.textContent = date.getHours();
+  minut.textContent = date.getMinutes();
+  second.textContent = date.getSeconds();
+};
+setInterval(() => {
+  var date = new Date();
+  weekDay.textContent = week[date.getDay()];
+  month.textContent = months[date.getMonth()];
+  monthDay.textContent = date.getDate();
+  hour.textContent =
+    date.getHours() > 10 ? date.getHours() : `0${date.getHours()}`;
+  minut.textContent =
+    date.getMinutes() > 10 ? date.getMinutes() : `0${date.getMinutes()}`;
+  second.textContent =
+    date.getSeconds() > 10 ? date.getSeconds() : `0${date.getSeconds()}`;
+}, 1000);
+
+//   Taskni HTML ga yozib beradigan funksiya
 
 writeTask();
 
@@ -33,10 +94,13 @@ addBtn.addEventListener("click", () => {
     var task = {
       id: Date.now(),
       task: inputTask.value,
+      notCheck: "display",
+      checkTask: "none",
       checkedClass: "notChecked",
       date: "8 - yanvar",
     };
     myDayDB.push(task);
+    searchTaskDB = [...myDayDB];
     localStorage.setItem("myDayDB", JSON.stringify(myDayDB));
     writeTask();
     inputTask.value = "";
@@ -47,8 +111,31 @@ addBtn.addEventListener("click", () => {
 
 const checked = (id) => {
   myDayDB.map((task) => {
-    return task.id == id ? (task.checkedClass = "checkedTask") : "notChecked";
+    return task.id == id
+      ? ((task.checkedClass = "checkedTask"),
+        (task.notCheck = "none"),
+        (task.checkTask = "display"))
+      : "notChecked";
   });
   localStorage.setItem("myDayDB", JSON.stringify(myDayDB));
   writeTask();
 };
+
+const deleteTask = (id) => {
+  filteredDB = myDayDB.filter((task) => {
+    return task.id != id;
+  });
+  myDayDB = filteredDB;
+  localStorage.setItem("myDayDB", JSON.stringify(myDayDB));
+  searchTaskDB = [...myDayDB];
+  writeTask();
+};
+
+searchInput.addEventListener("input", () => {
+  resultDb = myDayDB.filter((item) => {
+    return item.task.includes(searchInput.value);
+  });
+  console.log(resultDb);
+  searchTaskDB = resultDb;
+  writeTask();
+});
